6660145c52da7a03e38c9e7edfa4b8d7207b9e42
Your Name <k.soto@pucp.pe>
2021-06-27 21:52:25 -0500

    [on doing]

---
+++ op_pending/ft_op_bases.c
@@ -0,0 +1,109 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_op_bases.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ksoto <ksoto@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/22 22:39:09 by ksoto             #+#    #+#             */
+/*   Updated: 2021/06/24 06:04:06 by ksoto            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+/**
+ * print_octal - Converts a decimal num passed to the argument to an octal
+ * num
+ * @lista: The num to be converted
+ * Return: c of digit in octal num
+ */
+int print_octal(va_list lista)
+{
+	unsigned int num, c = 0, index = 0;
+	int arr[100];
+
+	num = va_arg(lista, int);
+	if (num < 9)
+	{
+		ft_putchar(num + '0');
+		c = 1;
+	}
+	else if (num >= 9)
+	{
+		while (num > 0)
+		{
+			arr[index] = num % 8;
+			num /= 8;
+			index++;
+		}
+	}
+	while (index--)
+	{
+		ft_putchar(arr[index] + '0');
+		c++;
+	}
+	return (c);
+}
+
+/**
+ * print_hexadecimal - Converts decimal to uppercase hexadecimal
+ * @n: The number to be converted
+ *
+ * Return: The number of digits printed
+ */
+int	print_hexadecimal(unsigned int n)
+{
+	int counter = 0;
+	char hex_digits[] = "0123456789abcdef";
+
+	if (n >= 16)
+		counter += print_hexadecimal(n / 16);
+	counter += ft_putchar(hex_digits[n % 16]);
+	return (counter);
+}
+
+/**
+ * print_HEXADECIMAL - Converts decimal to uppercase hexadecimal
+ * @n: The number to be converted
+ *
+ * Return: The number of digits printed
+ */
+int	print_HEXADECIMAL(unsigned int n)
+{
+	int counter = 0;
+	char hex_digits[] = "0123456789ABCDEF";
+
+	if (n >= 16)
+	{
+		counter += print_HEXADECIMAL(n / 16);
+	}
+	counter += write(1, &hex_digits[n % 16], 1);
+	return (counter);
+}
+
+/**
+ * print_hex - Converts decimal to uppercase hexadecimal
+ * @lista: The number of list to be converted
+ *
+ * Return: The number of digits printed
+ */
+int print_hex(va_list lista)
+{
+	unsigned int n = va_arg(lista, int);
+
+	return (print_hexadecimal(n));
+}
+
+/**
+ * print_HEX - Converts decimal to uppercase hexadecimal
+ * @lista: The number of list to be converted
+ *
+ * Return: The number of digits printed
+ */
+int print_HEX(va_list lista)
+{
+	unsigned int n = va_arg(lista, int);
+
+	return (print_HEXADECIMAL(n));
+}
---
+++ op_pending/select_functions.c
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   select_functions.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ksoto <ksoto@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/22 22:38:40 by ksoto             #+#    #+#             */
+/*   Updated: 2021/06/24 05:44:39 by ksoto            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+/**
+*print_char - print a char at printf
+*@lista: the main string passed to the function
+*Return: An integer
+*/
+int print_char(va_list lista)
+{
+	/*va_arg (va_list ap, type)*/
+	ft_putchar(va_arg(lista, int));
+	return (1);
+	/* returning true */
+}
+
+/**
+*print_str - print a char at printf
+*@lista: the main string passed to the function
+*Return: An integer
+*/
+int print_str(va_list lista)
+{
+	unsigned int count = 0;
+
+	char *str = va_arg(lista, char *);
+
+	if (!str || str == NULL)
+	{
+		ft_putchar('(');
+		ft_putchar('n');
+		ft_putchar('u');
+		ft_putchar('l');
+		ft_putchar('l');
+		ft_putchar(')');
+		return (6);
+	}
+	while (*str)
+	{
+		write(1, &(*str++), 1);
+		count++;
+	}
+	return (count);
+}
+
+/**
+ * select_function - structured list to work with it
+ * @format: string with operators
+ * @list: list of arguments to print
+ * Return: numbers of char printed
+ */
+int select_function(t_fields *str, va_list list)
+{
+	unsigned int i = str->idx, result = 0;
+
+	if (!str->format[i])
+		return (-1);
+	/* falta p & f */
+	t_fields operator[] = {
+			{'c', print_char},
+			{'s', print_str},
+	/*		{'d', print_int},
+			{'i', print_int},
+			{'u', print_unsigned},*/
+	//		{'o', print_octal},
+	/*		{'x', print_hex},
+			{'X', print_HEX},*/
+	//		{'b', print_bin},
+	//		{'S', print_Str_Ascii},
+	//		{'r', print_rev_str},
+	//		{'R', print_rot13},
+			{'\0', NULL}};
+
+	while (operator[i].op)
+	{
+		if (str->format[i] == operator[i].op)
+		{
+			// printf("here %c\n", operator[i].op);
+            // result += operator[i].f(list);
+			break;
+		}
+		i++;
+	}
+	if (result == 0)
+		return (0);
+	return (result);
+}

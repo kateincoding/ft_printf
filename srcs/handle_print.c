806e11951f8b7a6fa43bb0417cefa534fc07916c
Katherine Soto <kateincoding@gmail.com>
2021-07-10 06:32:44 -0500

    [fix case 0 when preciison = 0 ]

--- srcs/handle_print.c
+++ srcs/handle_print.c
@@ -6,7 +6,7 @@
 /*   By: ksoto <ksoto@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/04 00:01:26 by ksoto             #+#    #+#             */
-/*   Updated: 2021/07/10 05:30:35 by ksoto            ###   ########.fr       */
+/*   Updated: 2021/07/10 06:31:26 by ksoto            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,10 +40,10 @@ void	calculate_format_width(t_fields *str)
 
 void	print_before_before(t_fields *str, int i, int space)
 {
-	str->break_flag = 0;
 	if (str->space > 0 && (str->op == 'd' || str->op == 'i'))
 	{
 		i = 0;
+//		printf("\n ======here hey =====\n");
 		if (i < str->space)
 		{
 			str->counter += ft_putchar_fd(' ', str->fd);
@@ -57,8 +57,10 @@ void	print_before_before(t_fields *str, int i, int space)
 		i = 0;
 		if (str->precision > str->len)
 			space = str->precision;
-//			if (str->op == 'p' && str->null_flag == 1)
-//				space = space - 3;
+	//			if (str->op == 'p' && str->null_flag == 1)
+	//				space = space - 3;
+		else if (str->zero_value == 1 && str->final_width >= 1 && str->precision != 0)
+			space = 1;
 		else
 			space = str->len;
 		while (i < (str->final_width - space))

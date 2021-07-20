58e20340b6a8c3c0b3ed2441952151182f8ba079
Katherine Soto <kateincoding@gmail.com>
2021-07-20 15:59:57 -0400

    d

--- srcs/ft_conversion_diu.c
+++ srcs/ft_conversion_diu.c
@@ -6,54 +6,11 @@
 /*   By: ksoto <ksoto@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/01 05:39:10 by ksoto             #+#    #+#             */
-/*   Updated: 2021/07/20 11:40:51 by ksoto            ###   ########.fr       */
+/*   Updated: 2021/07/20 12:17:36 by ksoto            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
-void	size_bit(t_fields *str, int bit, int n)
-{
-	int	d;
-	int	x;
-
-	d = 1;
-	while ((bit / 10) != 0)
-	{
-		bit = bit / 10;
-		d = d * 10;
-	}
-	while (d >= 1)
-	{
-		printf("flag");
-		x = n / d;
-		str->counter += ft_putchar_fd(x + '0', str->fd);
-		n = n % d;
-		d = d / 10;
-	}
-}
-
-/*
-** print an integer
-*/
-
-void	print_body(t_fields *str, int n)
-{
-	int	bit = n;
-	int	o;
-
-	o = n % 10;
-	n = n / 10;
-	if (o < 0)
-	{
-		n = -n;
-		o = -o;
-	}
-	if (bit > 0)
-		size_bit(str, bit, n);
-	if (str->zero_value == 0 || (str->zero_value == 1 && str->precision != 0))
-		str->counter += ft_putchar_fd(o + '0', str->fd);
-}
 
 /*
 ** calculate integer len
@@ -96,7 +53,6 @@ void	calculate_int_len(t_fields *str, int num)
 		{
 			tmp = tmp / 10;
 			str->len++;
-//			printf("flag1\n");
 		}
 		if (tmp > -10)
 			str->len += 2;
@@ -113,7 +69,7 @@ void	calculate_int_len(t_fields *str, int num)
 		str->len = 0;
 }
 
-int		putnbr_di(long n, int fd)
+int	putnbr_di(long n, int fd)
 {
     int counter = 0;
 

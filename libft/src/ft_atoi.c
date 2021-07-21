c3f8cf3d03129516d91efb626a4259f2b8fb3c03
Katherine Soto <kateincoding@gmail.com>
2021-07-21 13:52:41 -0400

    [norme]

--- libft/src/ft_atoi.c
+++ libft/src/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: ksoto <ksoto@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/19 21:20:57 by ksoto             #+#    #+#             */
-/*   Updated: 2021/06/27 13:24:42 by ciglesia         ###   ########.fr       */
+/*   Updated: 2021/07/21 13:34:32 by ksoto            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,15 @@ int	ft_atoi(char *str)
 	i = 0;
 	sign = 1;
 	number = 0;
-	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t' ||
-							str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
+	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t'
+		|| str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
 		i++;
 	if (str[i] == '-' || str[i] == '+')
 	{
-		sign = str[i] == '-' ? -1 : 1;
+		if (str[i] == '-')
+			sign = -1;
+		else
+			sign = 1;
 		i++;
 	}
 	while (str[i] >= '0' && str[i] <= '9')
